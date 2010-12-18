2354965b4f8c95e2bfbec64edc8f3ed078423f83
Damien Leroy <damien.leroy@be-oi.be>
2010-12-18 18:35:04 +0100

    ticket4 started... to be continued when mail will worked

--- contest/views.py
+++ contest/views.py
@@ -72,7 +72,8 @@ def registration(request, template):
 			
 			if request.LANGUAGE_CODE == "fr":
 				mail_template = get_template("emails/fr/registration.txt")
-				context = Context({"name":cd["surname"]})
+				context = Context({"NAME":cd["firstname"]+" "+cd["surname"], "CONTEST": cd["contest_category"],
+				 					"CENTER_NAME": cd["semifinal_center"], "CENTER_DETAILS":reverse("regional-centers-fr")  })
 				send_mail("Inscription aux Olympiades d'Informatique", mail_template.render(context), "info@be-oi.be", [cd["email"]], fail_silently=True)
 				return HttpResponseRedirect(reverse("registration-confirm-fr", args=[cd["semifinal_center"].id])) 
 			else: 
