d49a2c47ebb492f334dc47d549f7af382d510f19
Damien Leroy <damien.leroy@uclouvain.be>
2010-12-20 13:35:14 +0100

    ndls mail sending updated

--- contest/views.py
+++ contest/views.py
@@ -78,7 +78,8 @@ def registration(request, template):
 				return HttpResponseRedirect(reverse("registration-confirm-fr", args=[cd["semifinal_center"].id])) 
 			else: 
 				mail_template = get_template("emails/nl/registration.txt")
-				context = Context({"name":cd["surname"]})
+				context = Context({"NAME":cd["firstname"]+" "+cd["surname"], "CONTEST": cd["contest_category"],
+				 					"CENTER_NAME": cd["semifinal_center"], "CENTER_DETAILS":reverse("regional-centers-nl")  })
 				send_mail("Inscription aux Olympiades d'Informatique", mail_template.render(context), "info@be-oi.be", [cd["email"]], fail_silently=True)
 				return HttpResponseRedirect(reverse("registration-confirm-nl", args=[cd["semifinal_center"].id])) 
 
