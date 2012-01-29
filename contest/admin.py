8af96997e861835f101cb71e4ba24654231de88f
Damien Leroy <damien.leroy@uclouvain.be>
2012-01-29 16:58:47 +0100

    unicode encoding support for csv export

--- contest/admin.py
+++ contest/admin.py
@@ -4,8 +4,38 @@ Administration interface options of ``contest`` application.
 """
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+import csv, codecs, cStringIO
 
 from beoi.contest.models import School, SemifinalCenter, Contestant, ResultSemifinal, ResultFinal, LANG_FR, LANG_NL
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 
 class SchoolAdmin(admin.ModelAdmin):
@@ -45,13 +75,12 @@ class ContestantAdmin(admin.ModelAdmin):
 	mark_as_checked.short_description = _("Mark selected contestants as manual checked")
 		
 	def export_as_csv(self, request, queryset):
-		import csv
 		from django.http import HttpResponse
 		
 		response = HttpResponse(mimetype='text/csv')
 		response['Content-Disposition'] = 'attachment; filename=contestants.csv'
 
-		writer = csv.writer(response)
+		writer = UnicodeWriter(response)
 		writer.writerow(['Name', 'Year', 'Lang', 'Signature'])
 		for contestant in queryset:
 			if contestant.language == LANG_FR: lang = "fr"
@@ -59,7 +88,7 @@ class ContestantAdmin(admin.ModelAdmin):
 			
 			writer.writerow([
 				contestant.surname+" "+contestant.firstname, 
-				contestant.year_study, 
+				str(contestant.year_study), 
 				lang, 
 				""
 			])
